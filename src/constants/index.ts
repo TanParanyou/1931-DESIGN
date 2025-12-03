48bb903fe96f01d81a76e1084c1f97fa76b0d3b4
Tan <130145434+TanParanyou@users.noreply.github.com>
2025-12-04 00:00:37 +0700

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,5 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+if (!API_BASE_URL) {
+    throw new Error('NEXT_PUBLIC_API_URL is not defined');
+}
