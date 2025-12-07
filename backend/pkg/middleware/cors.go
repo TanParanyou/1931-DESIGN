aa59c16876c6537f1884586883cd8c4ffc881505
Tan <130145434+TanParanyou@users.noreply.github.com>
2025-12-07 18:27:21 +0700

    Update cors.go

--- backend/pkg/middleware/cors.go
+++ backend/pkg/middleware/cors.go
@@ -1,24 +1,13 @@
 package middleware
 
 import (
-	"os"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func CORS() fiber.Handler {
-	allowOrigins := os.Getenv("ALLOWED_ORIGINS")
-	defaultOrigins := "http://localhost:3000, https://one931-design.onrender.com"
-
-	if allowOrigins == "" {
-		allowOrigins = defaultOrigins
-	} else {
-		allowOrigins = allowOrigins + ", " + defaultOrigins
-	}
-
 	return cors.New(cors.Config{
-		AllowOrigins: allowOrigins,
+		AllowOrigins: "*",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 	})
 }
