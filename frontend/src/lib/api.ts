bcb27f40d3047e50885e0affcd7417cedbd3a13b
Tan <130145434+TanParanyou@users.noreply.github.com>
2025-12-13 13:55:46 +0700

    Update api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
-// const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api';
-const API_URL = 'http://localhost:8080';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api';
+//const API_URL = 'http://localhost:8080';
 
 const api = axios.create({
     baseURL: API_URL + '/api',
